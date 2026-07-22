e9c892a5eb2a018f8342f2a9b5e37e725c6b6b34
emphibian <adarshrajput181@gmail.com>
2026-07-22 12:54:08 +0530

    Add cursor size & border size slider

--- app/src/main/java/com/example/cursorpad/SettingsScreen.kt
+++ app/src/main/java/com/example/cursorpad/SettingsScreen.kt
@@ -2,12 +2,15 @@ package com.example.cursorpad
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -15,17 +18,24 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
 fun SettingsScreen(
-    onBackPressed: () -> Unit
+    onBackPressed: () -> Unit = {}
 ) {
     Scaffold(
         topBar = {
@@ -35,7 +45,10 @@ fun SettingsScreen(
                     IconButton(
                         onClick = { onBackPressed() }
                     ) {
-                        Icon(imageVector = Icons.AutoMirrored.Default.ArrowBack, contentDescription = "Back")
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                            contentDescription = "Back"
+                        )
                     }
                 },
                 colors = TopAppBarDefaults.topAppBarColors(
@@ -45,12 +58,45 @@ fun SettingsScreen(
         }
     ) { innerPadding ->
         Column(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
                 .padding(innerPadding)
                 .padding(24.dp)
         ) {
-            Text("App Settings", fontSize = 22.sp, fontWeight = FontWeight.Bold)
-        }
+            var cursorSize by rememberSaveable { mutableStateOf(30f) }
+            var borderSize by rememberSaveable { mutableStateOf(2f) }
+            Text(
+                "Cursor Settings",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Column(modifier = Modifier.padding(vertical = 5.dp)) {
+                Text("Cursor Size: ${cursorSize.toInt()}", fontSize = 16.sp)
+                Spacer(
+                    modifier = Modifier.width(16.dp)
+                )
+                Slider(
+                    value = cursorSize,
+                    onValueChange = { cursorSize = it },
+                    valueRange = 20f..50f,
+                    steps = 30
+                )
+            }
+
+            Column(modifier = Modifier.padding(vertical = 5.dp)) {
+                Text("Border Size: ${borderSize.toInt()}", fontSize = 16.sp)
+                Slider(
+                    value = borderSize,
+                    onValueChange = { borderSize = it },
+                    valueRange = 0f..5f,
+                    steps = 4
+                )
+            }
+    }
 
     }
 }
